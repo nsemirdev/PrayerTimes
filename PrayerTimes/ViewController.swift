ec0677d8a36a898387141d3d8213c43cd4dadbbe
nsemirdev <nsemirdev@gmail.com>
2023-04-13 15:46:55 +0300

    Initial Commit

---
+++ PrayerTimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrayerTimes
+//
+//  Created by Emir Alkal on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
